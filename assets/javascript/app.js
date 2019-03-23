5a9008b83260ff7b3a70deb52f25bfa8dec59bca
Tony Kruso <tony.kruso@gmail.com>
2019-03-23 17:26:12 -0400

    added numbers to each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,52 +46,52 @@ countDown();
 
 var questions = [
     {
-        quest: "Who directed 'E.T. the Extra-Terrestrial' (1982)?",
+        quest: "1. Who directed 'E.T. the Extra-Terrestrial' (1982)?",
         answ: { 0: "Steven Spielberg", 1: "Quentin Tarantino", 2: "George Lucas"},
         cora: 1
     },
     {
-        quest: "Who starred in the film 1973 movie 'Enter The Dragon'",
+        quest: "2. Who starred in the film 1973 movie 'Enter The Dragon'",
         answ: { 0: "Jackie Chan", 1: "Jet Li", 2: "Bruce Lee"},
         cora: 3
     },
     {
-        quest: "In the movie 'Spaceballs', what are the Spaceballs attempting to steal from Planet Druidia?",
+        quest: "3. In the movie 'Spaceballs', what are the Spaceballs attempting to steal from Planet Druidia?",
         answ: { 0: "The Schwartz", 1: "Yogurt", 2: "Air"},
         cora: 3
     },
     {
-        quest: "Which of these movies did Jeff Bridges not star in?",
+        quest: "4. Which of these movies did Jeff Bridges not star in?",
         answ: { 0: "Tron: Legacy", 1: "The Hateful Eight", 2: "True Grit"},
         cora: 2
     },
     {
-        quest: "Who starred as Bruce Wayne and Batman in the Tim Burton's 1989 'Batman'",
+        quest: "5. Who starred as Bruce Wayne and Batman in the Tim Burton's 1989 'Batman'",
         answ: { 0: "Michael Keaton", 1: "Val Kilmer", 2: "George Clooney"},
         cora: 1
     },
     {
-        quest: "What is the highest grossing film of all time (without accounting for inflation)?",
+        quest: "6. What is the highest grossing film of all time (without accounting for inflation)?",
         answ: { 0: "Titanic", 1: "Avengers: Infinity War", 2: "Avatar"},
         cora: 3
     },
     {
-        quest: "What was the title of the first James Bond movie, released in 1962?",
+        quest: "7. What was the title of the first James Bond movie, released in 1962?",
         answ: { 0: "Goldfinger", 1: "Dr. No", 2: "Thunderball"},
         cora: 2
     },
     {
-        quest: "Which of these actors is NOT a part of the cast for the 1972 movie 'The Godfather'",
+        quest: "8. Which of these actors is NOT a part of the cast for the 1972 movie 'The Godfather'",
         answ: { 0: "Peter O'Toole", 1: "Robert Duvall", 2: "Al Pacino"},
         cora: 1
     },
     {
-        quest: "The 2002 film '28 Days Later' is mainly set in which European country?",
+        quest: "9. The 2002 film '28 Days Later' is mainly set in which European country?",
         answ: { 0: "United Kingdom", 1: "USA", 2: "France"},
         cora: 3
     },
     {
-        quest: "What is the 2nd Rule of Fight Club?",
+        quest: "10. What is the 2nd Rule of Fight Club?",
         answ: { 0: "Don't Hit Below the Belt!", 1: "Don't Talk about Fight Club", 2: "No Steal Toed Boots Allowed"},
         cora: 2
     }
